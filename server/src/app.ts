17bc95f9eeb4d31f348e32e93ce74fc1f3a8cdd2
xjq <xiajq01@mingyuanyun.com>
2022-10-11 20:14:46 +0800

    init server

---
+++ server/src/app.ts
@@ -0,0 +1,20 @@
+import Koa from 'koa'
+import Router from 'koa-router'
+
+const app = new Koa()
+
+const router = new Router()
+
+router.get('/', (ctx, next) => {
+  // ctx.router available
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.on('error', err => {
+  console.error('server error', err)
+});
+
+app.listen(3000)
