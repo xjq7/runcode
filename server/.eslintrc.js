986aa28fa7a98ae087a27bc400f01b83d38aa07c
xjq <xiajq01@mingyuanyun.com>
2022-10-12 12:52:53 +0800

    chore: 开发配置

---
+++ server/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  extends: ['standard-with-typescript', 'prettier'],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  rules: {},
+};
