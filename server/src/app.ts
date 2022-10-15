9c00b166cbbebea8aec7df7cd7e9f9ca2925a6f2
xjq <lebnizjoan@gmail.com>
2022-10-16 00:08:03 +0800

    update

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,7 +5,15 @@ import codeRouter from './routes/code';
 
 const app = new Koa();
 app.use(bodyParser());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: '*',
+    allowHeaders:
+      'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization',
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS',
+  })
+);
 
 app.use(codeRouter.routes()).use(codeRouter.allowedMethods());
 
