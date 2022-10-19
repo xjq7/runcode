eb94123f85ea2995c84ceda3ad36d65878cd1e03
xjq <xiajq01@mingyuanyun.com>
2022-10-19 15:56:05 +0800

    feat: 增加超时机制，增加输入

--- server/src/docker/index.ts
+++ server/src/docker/index.ts
@@ -1,6 +1,17 @@
 import Docker, { Container } from 'dockerode';
+import { Stream } from 'stream';
 import dockerConfig from '../config/docker';
 import { CodeEnv, CodeType, FileSuffix } from '../utils/type';
+import { RunCodeStatus } from '../routes/code';
+
+const DockerRunConfig = {
+  timeout: 6000,
+};
+
+enum DockerRunStatus {
+  running = 'running',
+  exited = 'exited',
+}
 
 const docker = new Docker({
   ...dockerConfig,
@@ -45,9 +56,138 @@ const imageMap: Record<CodeType, CodeDockerOption> = {
   },
 };
 
-export async function run({ type, code }: { type: CodeType; code: string }) {
+export async function run2(params: {
+  type: CodeType;
+  code: string;
+  stdin: string;
+}) {
+  const Error = {
+    output: '',
+    code: RunCodeStatus.error,
+    time: 0,
+    message: '执行时间超时!',
+  };
+
+  const result = Error;
+
+  const { code, type, stdin } = params;
+
+  const dockerOptions = imageMap[type];
+
+  if (!dockerOptions) return Error;
+
+  const { env, shell, fileSuffix } = dockerOptions;
+
   let removeContainer = () => {};
 
+  let bashCmd = `cat > code.${fileSuffix} << EOF ${code}`;
+  if (stdin) {
+    bashCmd += `cat > input.txt << EOF ${stdin}
+    ${shell} < input.txt`;
+  } else {
+    bashCmd += `${shell}`;
+  }
+
+  return await new Promise((resolve, reject) => {
+    docker.createContainer(
+      {
+        Image: env,
+        Cmd: ['bash', '-c', bashCmd],
+        StopTimeout: 6,
+        Tty: true,
+        AttachStdout: true,
+      },
+      function (_err, container?: Container) {
+        if (_err) reject(_err);
+        removeContainer = async () => {
+          try {
+            await container?.remove({ force: true });
+          } catch (error) {}
+        };
+
+        container?.start((_err) => {
+          if (_err) {
+            removeContainer();
+            reject(_err);
+          }
+          const handleOutput = async () => {
+            try {
+              let outputString: any = await container?.logs({
+                stdout: true,
+                stderr: true,
+              });
+
+              outputString = formatOutput(outputString.toString('utf8'));
+
+              const containerInfo = await container?.inspect();
+              const isRunning = containerInfo.State.Running;
+
+              const isTimeout = !!isRunning;
+
+              if (isTimeout) {
+                result.code = RunCodeStatus.timeout;
+              } else {
+                result.code = RunCodeStatus.success;
+                result.output = outputString;
+              }
+            } catch (error) {
+              removeContainer();
+              reject(error);
+            } finally {
+              removeContainer();
+              resolve(result);
+            }
+          };
+
+          container?.attach(
+            { stream: true, stdout: true, stderr: true },
+            function (_err, stream?: Stream) {
+              stream?.pipe(process.stdout);
+            }
+          );
+
+          const timeoutSig = setTimeout(handleOutput, DockerRunConfig.timeout);
+
+          container?.wait((status) => {
+            if (!status || status?.Status === DockerRunStatus.exited) {
+              clearTimeout(timeoutSig);
+              handleOutput();
+            }
+          });
+        });
+      }
+    );
+  });
+}
+
+function formatOutput(outputString: string): string {
+  if (outputString.length > 4200) {
+    outputString =
+      outputString.slice(0, 2000) +
+      outputString.slice(outputString.length - 2000);
+  }
+
+  outputString = encodeURI(outputString);
+
+  outputString = outputString.replace(
+    /%1B%5B.*?m.*?%1B%5BK|%1B%5B.*?m|%0D/g,
+    ''
+  );
+
+  let outputStringArr = outputString.split('%0A');
+  if (outputStringArr.length > 200) {
+    outputStringArr = outputStringArr
+      .slice(0, 100)
+      .concat(
+        ['%0A', '...' + encodeURI('数据太多,已折叠'), '%0A'],
+        outputStringArr.slice(outputStringArr.length - 100)
+      );
+  }
+
+  return outputStringArr.join('%0A');
+}
+
+export async function run({ type, code }: { type: CodeType; code: string }) {
   const Error = {
     output: '',
     code: 1,
@@ -71,17 +211,17 @@ export async function run({ type, code }: { type: CodeType; code: string }) {
         '-c',
         `cat > code.${fileSuffix} << EOF ${code} \
         ${shell}`,
+        // cat > input.txt << EOF ${stdin} \
+        // ${shell} < input.txt`,
       ],
       process.stdout,
-      { StopTimeout: 1 }
+      { StopTimeout: 5 }
     );
 
     const output = data[0] || {};
     result.code = output?.StatusCode;
 
     const container: Container = data[1];
-
-    removeContainer = () => container.remove();
     const readstream: any = await container.logs({
       stdout: true,
       stderr: true,
@@ -116,8 +256,6 @@ export async function run({ type, code }: { type: CodeType; code: string }) {
     result.code = 0;
   } catch (error) {
     console.log(error);
-  } finally {
-    removeContainer();
   }
   return result;
 }
--- server/src/routes/code.ts
+++ server/src/routes/code.ts
@@ -4,27 +4,46 @@ import { CodeType } from '../utils/type';
 
 const router = new Router({ prefix: '/code' });
 
+export enum RunCodeStatus {
+  success = 0,
+  timeout = 1,
+  error = 2,
+}
+
 router.post('/run', async (ctx) => {
   const body = ctx.request.body ?? {};
 
-  const { code, type } = body;
+  const { code, type, stdin } = body;
 
-  if (typeof code !== 'string' || typeof type !== 'string') {
+  if (
+    typeof code !== 'string' ||
+    typeof type !== 'string' ||
+    (stdin && typeof stdin !== 'string')
+  ) {
     ctx.throw(400, '参数有误!');
     return;
   }
 
   const wrapCode = '\n' + decodeURI(code) + '\n' + 'EOF' + '\n';
+  const wrapStdin =
+    '\n' + decodeURI((stdin as string) || '') + '\n' + 'EOF' + '\n';
 
-  const output = await docker.run({
-    type: type as CodeType,
-    code: wrapCode,
-  });
-
-  ctx.body = {
-    code: 0,
-    data: output,
-  };
+  try {
+    const output = await docker.run2({
+      type: type as CodeType,
+      code: wrapCode,
+      stdin: wrapStdin,
+    });
+    ctx.body = {
+      code: 0,
+      data: output,
+    };
+  } catch (error) {
+    ctx.body = {
+      code: 1,
+      message: JSON.stringify(error),
+    };
+  }
 });
 
 export default router;
