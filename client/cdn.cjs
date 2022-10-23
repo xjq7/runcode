74484cbe38a8420896de3ff478577471276ca638
xjq <lebnizjoan@gmail.com>
2022-10-24 01:51:47 +0800

    chore: cdn 部署 忽略 assets 外部文件

--- client/cdn.cjs
+++ client/cdn.cjs
@@ -16,8 +16,8 @@ const [AccessKeyId, AccessKeySecret, bucket, region] = process.argv.slice(2);
     await deploy({
       ossConfig,
       //  最好同时配置staticPath,ossPath,确定上传文件路径以及存储路径
-      staticPath: path.resolve(__dirname, 'dist'), // 默认为根路径
-      ossPath: 'runcode/' + dayjs().format('YYYY-MM-DD'), // oss存储路径,默认是根路径,
+      staticPath: path.resolve(__dirname, 'dist/assets'), // 默认为根路径
+      ossPath: 'runcode/' + dayjs().format('YYYY-MM-DD') + '/assets', // oss存储路径,默认是根路径,
       recursion: true, // 递归上传,默认为true,文件夹下所有文件递归上传
     });
   } catch (error) {
