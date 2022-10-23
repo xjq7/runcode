0f318247ced9e78c71338f2698234bbe0ddcb1e0
xjq <lebnizjoan@gmail.com>
2022-10-24 01:45:08 +0800

    fix(monaca editor): 线上 worker 文件加载跨域报错问题

--- client/src/userWorker.ts
+++ client/src/userWorker.ts
@@ -1,14 +1,12 @@
 import * as monaco from 'monaco-editor';
-import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
-import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
 
 // @ts-ignore
 self.MonacoEnvironment = {
   getWorker(_: any, label: string) {
     if (label === 'typescript' || label === 'javascript') {
-      return new tsWorker();
+      return new Worker(window.location.origin + '/ts.worker.js?worker');
     }
-    return new editorWorker();
+    return new Worker(window.location.origin + '/editor.worker.js?worker');
   },
 };
 
