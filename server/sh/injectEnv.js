f1c685d4e18c33dc1f170b6ccef9a338855a36e2
xjq <lebnizjoan@gmail.com>
2022-10-30 23:41:49 +0800

    chore: fix inject env

--- server/sh/injectEnv.js
+++ server/sh/injectEnv.js
@@ -5,9 +5,8 @@ const args = process.argv.slice(2);
 (async function () {
   let filehandle = null;
   try {
-    filehandle = await fs.open('.env', 'r+');
-    await filehandle.writeFile('');
-    await filehandle.write(args.join('\n'));
+    await fs.unlink('.env');
+    await fs.writeFile('.env', args.join('\n'));
   } catch (err) {
     console.log(err);
     process.exit(1);
