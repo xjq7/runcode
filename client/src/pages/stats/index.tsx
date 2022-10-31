1013d8ab7419120c8f97a7b2302e1af8c951ee35
xjq <xiajq01@mingyuanyun.com>
2022-10-31 16:01:15 +0800

    test: fix client

--- client/src/pages/stats/index.tsx
+++ client/src/pages/stats/index.tsx
@@ -1,8 +1,6 @@
-import { uniqBy } from 'lodash';
 import { useEffect, useState } from 'react';
 import styles from './index.module.less';
 import { getStat, Stat } from './service';
-import { Chart, Util } from '@antv/g2';
 import dayjs from 'dayjs';
 
 function Component() {
@@ -25,59 +23,6 @@ function Component() {
     fetchStat();
   }, []);
 
-  useEffect(() => {
-    return;
-    const chart = new Chart({
-      container: 'container',
-      autoFit: true,
-      height: 500,
-    });
-    chart.data(stats.os);
-
-    chart.coordinate('theta', {
-      radius: 0.75,
-    });
-    chart.tooltip({
-      showMarkers: false,
-    });
-
-    const interval = chart
-      .interval()
-      .adjust('stack')
-      .position('value')
-      .color('type', ['#063d8a', '#1770d6', '#47abfc', '#38c060'])
-      .style({ opacity: 0.4 })
-      .state({
-        active: {
-          style: (element) => {
-            const shape = element.shape;
-            return {
-              matrix: Util.zoom(shape, 1.1),
-            };
-          },
-        },
-      })
-      .label('type', (val) => {
-        const opacity = val === '四线及以下' ? 1 : 0.5;
-        return {
-          offset: -30,
-          style: {
-            opacity,
-            fill: 'white',
-            fontSize: 12,
-            shadowBlur: 2,
-            shadowColor: 'rgba(0, 0, 0, .45)',
-          },
-          content: (obj) => {
-            return obj.type + '\n' + obj.value + '%';
-          },
-        };
-      });
-
-    chart.interaction('os-stats');
-
-    chart.render();
-  }, [stats]);
   return (
     <div className={styles.container}>
       <div>
