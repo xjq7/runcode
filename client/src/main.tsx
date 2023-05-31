91de57ec81ca0684a8f81fca3d674081c512ff2e
xjq <lebnizjoan@gmail.com>
2023-05-31 18:56:51 +0800

    feat: 接入 监控 SDK

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,16 @@ import rem from '~utils/rem';
 import './userWorker';
 import 'xterm/css/xterm.css';
 import './tailwind.output.css';
+import Reporter, { Browser } from '@mtrjs/sdk';
 
+const reporter = new Reporter({
+  env: 'prod',
+  dsn: 'https://mtr-api.xjq.icu',
+  appId: '6be4bd000f6ad238',
+  plugins: [new Browser()],
+});
+
+reporter.init();
 rem();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
